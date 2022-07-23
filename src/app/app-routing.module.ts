155b9a7fe7baa524f7e677707cab8e1c61e14187
Mauricio Bacelis <mbacelis_@hotmail.com>
2022-07-23 13:45:18 -0500

    Se implementa SideMenu que apunta a todas las rutas hijas creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'reactive', loadChildren: () => import('./reactive/reactive.module').then(m => m.ReactiveModule)},
   { path: 'template', loadChildren: () => import('./template/template.module').then(m => m.TemplateModule) },
+  { path: 'reactive', loadChildren: () => import('./reactive/reactive.module').then(m => m.ReactiveModule)},
   { path: '**', redirectTo: 'template'}
 ];
 
