8ad9b6eb6d7093c77b8b084f30b265d10ce4b377
Mauricio Bacelis <mbacelis_@hotmail.com>
2022-08-26 14:57:17 -0500

    Eliminar elementos creados de forma dinámica

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+
+interface Persona{
+  nombre: string;
+  favoritos: Favorito[]
+}
+
+interface Favorito{
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,16 +16,31 @@ import { NgForm } from '@angular/forms';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+
+  persona: Persona = {
+    nombre: 'Mauricio',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear'},
+      { id: 2, nombre: 'DeadthStranding'},
+    ]
+  }
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   guardar() {
     console.log('Formulario posteado');
     
+  }
+
+  //Eliminar elemento por id
+  borrarFavorito(id: number) {
+    var index = this.persona
+      .favoritos
+      .findIndex(f => f.id === id);
+    
+    if (index >= 0)
+      this.persona.favoritos.splice(index, 1);
   }
 
 }
