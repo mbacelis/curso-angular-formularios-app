00a4f1dbe038768be9df1939e001aeeee56abc66
Mauricio Bacelis <mbacelis_@hotmail.com>
2022-08-27 18:43:17 -0500

    Se agregan validaciones básicas síncronas

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -16,9 +16,9 @@ export class BasicosComponent {
   // });
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['RTX 4080ti'],
-    precio: 0,
-    existencias: 0
+    nombre: ['RTX 4080ti', [Validators.required, Validators.minLength(3)]],
+    precio: [0, [Validators.required, Validators.min(0)]],
+    existencias: [0, [Validators.required, Validators.min(0)]]
   })
 
   constructor(private fb: FormBuilder) { }
