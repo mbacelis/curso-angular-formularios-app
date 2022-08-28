905edae78881b9a569aa00d91cd022da2ba110f9
Mauricio Bacelis <mbacelis_@hotmail.com>
2022-08-27 20:21:46 -0500

    Se agrega formulario reactivo para el ejemplo de dinamicos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]]
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  validacionCampo(campo: string) {
+    return this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched;
   }
 
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+    
+  }
 }
