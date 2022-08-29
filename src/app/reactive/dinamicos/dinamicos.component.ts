ba8136b6f8bc0cabbb1309d1af4aec9a8da8fe9b
Mauricio Bacelis <mbacelis_@hotmail.com>
2022-08-28 19:35:41 -0500

    Ejemplo con FormArray

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, Validator, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -10,14 +10,26 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class DinamicosComponent {
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: [, [Validators.required, Validators.minLength(3)]]
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array([
+      ['Metal Gear'],
+      ['Death Stranding']
+    ], Validators.required)
   });
+
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
+}
 
   constructor(private fb: FormBuilder) { }
 
   validacionCampo(campo: string) {
     return this.miFormulario.controls[campo].errors &&
       this.miFormulario.controls[campo].touched;
+  }
+
+  agregarFavorito(favorito: string) {
+    
   }
 
   guardar() {
